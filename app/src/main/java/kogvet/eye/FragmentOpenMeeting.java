58ae0bbb875c38f7b2f3d8cfff973db6fa14417e
Tommy Olsson <kv15ton@cs.umu.se>
2018-04-05 14:01:29 +0200

    Merge

---
+++ app/src/main/java/kogvet/eye/FragmentOpenMeeting.java
@@ -0,0 +1,13 @@
+package kogvet.eye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentOpenMeeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_open_meeting);
+    }
+}
