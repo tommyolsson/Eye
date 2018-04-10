dc80c9bd92a426b3de1ee5e43457ed914d3e7613
Adrian Hellqvist <c12aht@cs.umu.se>
2018-04-10 16:22:02 +0200

    changed fetch date for demonstration

--- app/src/main/java/kogvet/eye/MainActivity.java
+++ app/src/main/java/kogvet/eye/MainActivity.java
@@ -170,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
     /* Use Volley to make an HTTP request to the /me endpoint from MS Graph using an access token */
     private void callGraphAPI() {
         /*Update Url to request events from today to one month ahead*/
-        startDate = LocalDate.now().toString();
+        startDate = LocalDate.now().minusDays(1).toString(); //MINUS EN DAG FÖR IRIS VISNING.
         endDate = LocalDate.now().plusMonths(1).toString();
         msGraph_URL = "https://graph.microsoft.com/beta/me/calendar/calendarView?startDateTime="+startDate+"T00:00:00.0000000&endDateTime="+endDate+"T00:00:00.0000000&$orderby=start/dateTime";
 //        msGraph_URL = buildUrl();
