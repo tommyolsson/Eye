a49662160626ce2e1943f65112bd8a204772a57e
Adrian Hellqvist <c12aht@cs.umu.se>
2018-05-24 16:31:46 +0200

    splash screen and changes

--- app/src/main/java/kogvet/eye/SplashscrenActivity.kt
+++ app/src/main/java/kogvet/eye/SplashscrenActivity.kt
@@ -1,21 +1,79 @@
 package kogvet.eye
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
+import android.os.Handler
 
+class SplashscreenActivity: AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 1500 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+    /*val TIME_OUT: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        Handler().postDelayed(Runnable {
+            kotlin.run {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, TIME_OUT)
+    }
+*/
+}
+
+/*
 class SplashscreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
 
         // continue with your regular implementation
+
 
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
 
+
+
     }
 
-}
+}*/
